cb7907e1b966fb9fdc9710bc8adca2008f7bd5b4
Jose Figueroa <60245427+edik-jackov@users.noreply.github.com>
2020-03-10 20:02:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML lang="es">
+<HEAD>
+<meta charset="UTF-8">
+<meta NAME="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title> tarea 6</title>
+<link rel="stylesheet" href="css/style.css>
+<body>
+<?php
+
+echo"<h1> Hola Mundo</h1>";
+
+?>
+</body>
+</HTML>
